c12c9eeed41dc15503b349af15337d9e370b02ef
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2025-11-07 20:12:27 -0800

    Add notebook with initial EDA on dataset

---
+++ visual_eda/visual_eda/spark.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import time
+from contextlib import contextmanager
+from pathlib import Path
+
+from pyspark.sql import SparkSession
+
+os.environ["PYSPARK_PYTHON"] = sys.executable
+os.environ["PYSPARK_DRIVER_PYTHON"] = sys.executable
+
+
+def get_spark(
+    cores=os.environ.get("PYSPARK_EXECUTOR_CORES", os.cpu_count()),
+    memory=os.environ.get("PYSPARK_DRIVER_MEMORY", "20g"),
+    local_dir=os.environ.get("SPARK_LOCAL_DIR", "/mnt/scratch/tmp"),
+    app_name="app",
+    **kwargs,
+):
+    """Get a spark session for a single driver."""
+    local_dir = f"{local_dir}/{int(time.time())}"
+    Path(local_dir).mkdir(parents=True, exist_ok=True)
+    builder = (
+        SparkSession.builder.config("spark.driver.memory", memory)
+        .config("spark.executor.cores", cores)
+        .config("spark.sql.execution.arrow.pyspark.enabled", "true")
+        .config("spark.driver.maxResultSize", "8g")
+        .config("spark.local.dir", local_dir)
+    )
+    for k, v in kwargs.items():
+        builder = builder.config(k, v)
+    return builder.appName(app_name).master(f"local[{cores}]").getOrCreate()
+
+
+@contextmanager
+def spark_resource(*args, **kwargs):
+    """A context manager for a spark session."""
+    spark = None
+    try:
+        spark = get_spark(*args, **kwargs)
+        yield spark
+    finally:
+        if spark is not None:
+            spark.stop()
